e2b6d91cf39c22e8fe885e5a11bac0e7a7bd41a7
Panos Mavrogiorgos <pmav99@gmail.com>
2024-06-29 23:34:41 +0300

    feat: Add `compare_models_app.py`

---
+++ compare_models_app.py
@@ -0,0 +1,97 @@
+from __future__ import annotations
+
+import logging
+import typing as T
+
+import holoviews as hv
+import pandas as pd
+import panel as pn
+
+from seareport_skill import load_stats
+from seareport_skill import settings
+
+logging.basicConfig(level=10)
+logger = logging.getLogger()
+
+pn.extension()
+
+versions = pn.widgets.MultiSelect(
+    name="Version",
+    options=settings.VERSIONS,
+    sizing_mode="stretch_width",
+    size=6,
+)
+metrics = pn.widgets.MultiSelect(
+    name="Metrics",
+    options=settings.METRICS,
+    sizing_mode="stretch_width",
+    size=15,
+)
+
+if pn.state.location:
+    pn.state.location.sync(versions, {"value": versions.name})
+    pn.state.location.sync(metrics, {"value": metrics.name})
+
+
+def _get_stats(versions_val: list[str], metrics_val: list[str]) -> pd.DataFrame:
+    stats = load_stats()
+    stats = stats[stats.version.isin(versions_val)]
+    stats = stats[metrics_val + ["version"]]
+    stats = T.cast(pd.DataFrame, stats)
+    return stats
+
+
+def _plot_metric(stats: pd.DataFrame, metric: str) -> hv.BoxWhisker:
+    plot = stats.hvplot.box(y=metric, by="version")
+    plot = plot.opts(
+        ylabel="",
+        invert_axes=True,
+        title=metric,
+        show_grid=True,
+        tools=["hover"],
+        outlier_radius=0.002,
+        height=100 + 20 * stats.version.nunique(),
+    )
+    return plot
+
+
+def _plot_table(stats: pd.DataFrame, metric: str) -> hv.Table:
+    df = (
+        stats[[metric, "version"]]
+        .groupby("version")
+        .describe([0.05, 0.25, 0.5, 0.75, 0.95])
+        .round(3)
+    )
+    df.columns = df.columns.get_level_values(1)
+    df = df.reset_index()
+    table = df.hvplot.table().opts(height=100 + 20 * stats.version.nunique())
+    return table
+
+
+@pn.depends(versions, metrics)
+def show_metrics(versions_val: list[str], metrics_val: list[str]):
+    if not versions_val:
+        versions_val = list(settings.VERSIONS.values())
+    if not metrics_val:
+        metrics_val = list(settings.METRICS.values())
+    stats = _get_stats(versions_val=versions_val, metrics_val=metrics_val)
+    plots = []
+    for metric in metrics_val:
+        plots.extend(
+            [
+                _plot_metric(stats=stats, metric=metric),
+                _plot_table(stats=stats, metric=metric),
+            ]
+        )
+    return hv.Layout(plots).cols(2)  # .opts(sizing_mode="stretch_width")
+
+
+template = pn.template.MaterialTemplate(
+    title="Metrics Table",
+    sidebar=[versions, metrics],
+    sidebar_width=430,
+    main=[
+        show_metrics,
+    ],
+)
+template.servable()
--- seareport_skill/__init__.py
+++ seareport_skill/__init__.py
@@ -1,24 +1,47 @@
 from __future__ import annotations
 
+import functools
+import pathlib
+import typing as T
+
 import geodatasets
 import geopandas as gp
 import pandas as pd
-import shapely
+import shapely.geometry
 
 __all__: list[str] = [
+    "load_countries",
     "load_model_stats",
+    "load_stats",
 ]
 
 
+@functools.cache
 def load_countries() -> gp.GeoDataFrame:
     path = geodatasets.get_path("naturalearth_land")
     countries = gp.read_file(path)
     return countries
 
 
+@functools.cache
 def load_model_stats(model_version) -> pd.DataFrame:
     df = pd.read_parquet(f"assets/{model_version}.parquet").astype(float).sort_index()
     return df
+
+
+@functools.cache
+def load_stats() -> pd.DataFrame:
+    dataframes = []
+    for pqfile in sorted(pathlib.Path("assets").glob("v*.parquet")):
+        version = pqfile.stem
+        df = load_model_stats(version)
+        # XXX Normalize values: This should be done directly in the skill calculations
+        df = df[(df > -2) & (df < 2)]
+        df = df.assign(version=version)
+        df["version"] = df.version.astype("category")
+        dataframes.append(df)
+    stats = pd.concat(dataframes).sort_values(["version"], ascending=False)
+    return T.cast(pd.DataFrame, stats)
 
 
 def find_ocean_for_station(station, oceans_df, xstr="longitude", ystr="latitude"):
